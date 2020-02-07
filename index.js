d5842d63a11f610203b750d263857cdcf3bafa21
walze <wallace.fares@webedia-group.com>
2020-02-07 11:22:13 -0300

    typos

---
+++ index.js
@@ -0,0 +1,8 @@
+// sem for/while loops
+// sem ifs
+// função tem que ser inline, sem blocos
+// Sem efeitos colaterais
+// nenhuma atribuição nas funções
+// apenas funciona com 0 ou 1 args
+// tudo o que você fizer, tem que usar uma função
+// sem async
